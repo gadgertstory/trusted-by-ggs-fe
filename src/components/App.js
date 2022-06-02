7f9b5146d7cb68a9a57107f98da0fc4779913f63
TutyatornS <tutyatorn.sa@gadgetstory.co.th>
2022-06-02 18:28:00 +0700

    update code

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+
+const App = () => {
+    return (
+        <h1>
+            <Header />
+        </h1>
+    );
+};
+
+export default App;
