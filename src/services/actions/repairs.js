39978efff34f4a5114bd722a948ca88d4bc625f6
TutyatornS <tutyatorn.sa@gadgetstory.co.th>
2022-09-29 10:47:34 +0700

    refactor: :recycle: refactor code export file xlsx

--- src/services/actions/repairs.js
+++ src/services/actions/repairs.js
@@ -4,8 +4,6 @@ import {
     FETCH_ALL_REPAIR_FAIL,
     REQUEST_REPAIR_SEARCH_SUCCESS,
     REQUEST_REPAIR_SEARCH_FAIL,
-    REQUEST_REPAIR_EXPORT_SUCCESS,
-    REQUEST_REPAIR_EXPORT_FAIL,
 } from "./types";
 
 import Repair from "../../middleware/repair";
@@ -70,47 +68,6 @@ export const RepairRequestSearch = (search) => async (dispatch) => {
 
             dispatch({
                 type: REQUEST_REPAIR_SEARCH_FAIL,
-            });
-
-            dispatch({
-                type: SET_MESSAGE,
-                payload: message,
-            });
-
-            return (
-                Promise.reject(),
-                actionHandler({
-                    error: message,
-                })
-            );
-        });
-};
-
-
-export const RepairRequestExport = (value) => async (dispatch) => {
-    // Calling the server 
-    await Repair.RepairRequestExport(value)
-        .then((response) => {
-            return response.data;
-        })
-
-        .then((responseJson) => {
-            dispatch({
-                type: REQUEST_REPAIR_EXPORT_SUCCESS,
-                payload: responseJson,
-            });
-            return responseJson;
-        })
-        .catch((error) => {
-            const message =
-                (error.response &&
-                    error.response.data &&
-                    error.response.data.message) ||
-                error.message ||
-                error.toString();
-
-            dispatch({
-                type: REQUEST_REPAIR_EXPORT_FAIL,
             });
 
             dispatch({
